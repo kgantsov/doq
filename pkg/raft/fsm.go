36924243cc4215f984d1a8eba6f2f11588193d59
Kostiantyn Hantsov <k.gantsov@gmail.com>
2024-08-26 18:31:50 +0200

    Refactor fsm.Apply method

--- pkg/raft/fsm.go
+++ pkg/raft/fsm.go
@@ -37,177 +37,201 @@ type FSMResponse struct {
 func (f *FSM) Apply(raftLog *raft.Log) interface{} {
 	var c Command
 	if err := json.Unmarshal(raftLog.Data, &c); err != nil {
-		panic(fmt.Sprintf("failed to unmarshal command: %s", err.Error()))
+		return &FSMResponse{QueueName: c.QueueName, error: err}
 	}
 
 	switch c.Op {
 	case "enqueue":
-		queue, err := f.queueManager.GetQueue(c.QueueName)
-		if err != nil {
-			return &FSMResponse{
-				QueueName: c.QueueName,
-				error:     fmt.Errorf("Failed to get a queue: %s", c.QueueName),
-			}
-		}
-
-		msg, err := queue.Enqueue(c.Group, c.Priority, c.Content)
-
-		log.Debug().Msgf("Node %s Enqueued a message: %+v %v", f.NodeID, msg, err)
-
-		if err != nil {
-			return &FSMResponse{
-				QueueName: c.QueueName,
-				error:     fmt.Errorf("Failed to enqueue a message to a queue: %s", c.QueueName),
-			}
-		}
-
-		return &FSMResponse{
-			QueueName: c.QueueName,
-			ID:        msg.ID,
-			Priority:  msg.Priority,
-			Content:   msg.Content,
-			error:     nil,
-		}
+		return f.enqueueApply(c)
 	case "dequeue":
-		q, err := f.queueManager.GetQueue(c.QueueName)
-		if err != nil {
-			return &FSMResponse{
-				QueueName: c.QueueName,
-				error:     fmt.Errorf("Failed to get a queue: %s", c.QueueName),
-			}
-		}
-
-		msg, err := q.Dequeue(c.Ack)
-
-		log.Debug().Msgf("Node %s Dequeued a message: %+v %v", f.NodeID, msg, err)
-
-		if err != nil {
-			if err == queue.ErrEmptyQueue {
-				return &FSMResponse{
-					QueueName: c.QueueName,
-					error:     fmt.Errorf("Queue is empty: %s", c.QueueName),
-				}
-			}
-			return &FSMResponse{
-				QueueName: c.QueueName,
-				error:     fmt.Errorf("Failed to dequeue a message from a queue: %s", c.QueueName),
-			}
-		}
-
-		return &FSMResponse{
-			QueueName: c.QueueName,
-			ID:        msg.ID,
-			Priority:  msg.Priority,
-			Content:   msg.Content,
-			error:     nil,
-		}
+		f.dequeueApply(c)
 	case "ack":
-		q, err := f.queueManager.GetQueue(c.QueueName)
-		if err != nil {
-			return &FSMResponse{
-				QueueName: c.QueueName,
-				error:     fmt.Errorf("Failed to get a queue: %s", c.QueueName),
-			}
-		}
-
-		err = q.Ack(c.ID)
-
-		log.Debug().Msgf("Node %s Acked a message: %v", f.NodeID, err)
-
-		if err != nil {
-			if err == queue.ErrEmptyQueue {
-				return &FSMResponse{
-					QueueName: c.QueueName,
-					error:     fmt.Errorf("Queue is empty: %s", c.QueueName),
-				}
-			} else if err == queue.ErrMessageNotFound {
-				return &FSMResponse{
-					QueueName: c.QueueName,
-					error:     fmt.Errorf("Message not found: %s", c.QueueName),
-				}
-			}
-			return &FSMResponse{
-				QueueName: c.QueueName,
-				error:     fmt.Errorf("Failed to ack a message from a queue: %s", c.QueueName),
-			}
-		}
-
-		return &FSMResponse{
-			QueueName: c.QueueName,
-			ID:        c.ID,
-			error:     nil,
-		}
+		f.ackApply(c)
 	case "updatePriority":
-		q, err := f.queueManager.GetQueue(c.QueueName)
-		if err != nil {
-			return &FSMResponse{
-				QueueName: c.QueueName,
-				error:     fmt.Errorf("Failed to get a queue: %s", c.QueueName),
-			}
-		}
-
-		err = q.UpdatePriority(c.ID, c.Priority)
-
-		log.Debug().Msgf(
-			"Node %s Updated priority for a message: %d %d %v",
-			f.NodeID,
-			c.ID,
-			c.Priority,
-			err,
-		)
-
-		if err != nil {
-			if err == queue.ErrEmptyQueue {
-				return &FSMResponse{
-					QueueName: c.QueueName,
-					error:     fmt.Errorf("Queue is empty: %s", c.QueueName),
-				}
-			}
-			return &FSMResponse{
-				QueueName: c.QueueName,
-				error:     fmt.Errorf("Failed to dequeue a message from a queue: %s", c.QueueName),
-			}
-		}
-
-		return &FSMResponse{
-			QueueName: c.QueueName,
-			ID:        c.ID,
-			Priority:  c.Priority,
-			Content:   "",
-			error:     nil,
-		}
+		f.updatePriorityApply(c)
 	case "createQueue":
-		_, err := f.queueManager.Create(c.QueueType, c.QueueName)
-		if err != nil {
-			return &FSMResponse{
-				QueueName: c.QueueName,
-				error:     fmt.Errorf("Failed to create a queue: %s", c.QueueName),
-			}
-		}
-
-		log.Debug().Msgf("Node %s Created a queue: %s", f.NodeID, c.QueueName)
-		return &FSMResponse{
-			QueueName: c.QueueName,
-			error:     nil,
-		}
+		return f.createQueueApply(c)
 	case "deleteQueue":
-		err := f.queueManager.Delete(c.QueueName)
-		if err != nil {
-			return &FSMResponse{
-				QueueName: c.QueueName,
-				error:     fmt.Errorf("Failed to delete a queue: %s", c.QueueName),
-			}
-		}
-
-		log.Debug().Msgf("Node %s Deleted a queue: %s", f.NodeID, c.QueueName)
-
-		return &FSMResponse{
-			QueueName: c.QueueName,
-			error:     nil,
-		}
+		return f.deleteQueueApply(c)
 	}
 
 	return &FSMResponse{QueueName: c.QueueName, error: fmt.Errorf("Unknown command: %s", c.Op)}
+}
+
+func (f *FSM) enqueueApply(c Command) *FSMResponse {
+	queue, err := f.queueManager.GetQueue(c.QueueName)
+	if err != nil {
+		return &FSMResponse{
+			QueueName: c.QueueName,
+			error:     fmt.Errorf("Failed to get a queue: %s", c.QueueName),
+		}
+	}
+
+	msg, err := queue.Enqueue(c.Group, c.Priority, c.Content)
+
+	log.Debug().Msgf("Node %s Enqueued a message: %+v %v", f.NodeID, msg, err)
+
+	if err != nil {
+		return &FSMResponse{
+			QueueName: c.QueueName,
+			error:     fmt.Errorf("Failed to enqueue a message to a queue: %s", c.QueueName),
+		}
+	}
+
+	return &FSMResponse{
+		QueueName: c.QueueName,
+		ID:        msg.ID,
+		Priority:  msg.Priority,
+		Content:   msg.Content,
+		error:     nil,
+	}
+}
+
+func (f *FSM) dequeueApply(c Command) *FSMResponse {
+	q, err := f.queueManager.GetQueue(c.QueueName)
+	if err != nil {
+		return &FSMResponse{
+			QueueName: c.QueueName,
+			error:     fmt.Errorf("Failed to get a queue: %s", c.QueueName),
+		}
+	}
+
+	msg, err := q.Dequeue(c.Ack)
+
+	log.Debug().Msgf("Node %s Dequeued a message: %+v %v", f.NodeID, msg, err)
+
+	if err != nil {
+		if err == queue.ErrEmptyQueue {
+			return &FSMResponse{
+				QueueName: c.QueueName,
+				error:     fmt.Errorf("Queue is empty: %s", c.QueueName),
+			}
+		}
+		return &FSMResponse{
+			QueueName: c.QueueName,
+			error:     fmt.Errorf("Failed to dequeue a message from a queue: %s", c.QueueName),
+		}
+	}
+
+	return &FSMResponse{
+		QueueName: c.QueueName,
+		ID:        msg.ID,
+		Priority:  msg.Priority,
+		Content:   msg.Content,
+		error:     nil,
+	}
+}
+
+func (f *FSM) ackApply(c Command) *FSMResponse {
+	q, err := f.queueManager.GetQueue(c.QueueName)
+	if err != nil {
+		return &FSMResponse{
+			QueueName: c.QueueName,
+			error:     fmt.Errorf("Failed to get a queue: %s", c.QueueName),
+		}
+	}
+
+	err = q.Ack(c.ID)
+
+	log.Debug().Msgf("Node %s Acked a message: %v", f.NodeID, err)
+
+	if err != nil {
+		if err == queue.ErrEmptyQueue {
+			return &FSMResponse{
+				QueueName: c.QueueName,
+				error:     fmt.Errorf("Queue is empty: %s", c.QueueName),
+			}
+		} else if err == queue.ErrMessageNotFound {
+			return &FSMResponse{
+				QueueName: c.QueueName,
+				error:     fmt.Errorf("Message not found: %s", c.QueueName),
+			}
+		}
+		return &FSMResponse{
+			QueueName: c.QueueName,
+			error:     fmt.Errorf("Failed to ack a message from a queue: %s", c.QueueName),
+		}
+	}
+
+	return &FSMResponse{
+		QueueName: c.QueueName,
+		ID:        c.ID,
+		error:     nil,
+	}
+}
+
+func (f *FSM) updatePriorityApply(c Command) *FSMResponse {
+	q, err := f.queueManager.GetQueue(c.QueueName)
+	if err != nil {
+		return &FSMResponse{
+			QueueName: c.QueueName,
+			error:     fmt.Errorf("Failed to get a queue: %s", c.QueueName),
+		}
+	}
+
+	err = q.UpdatePriority(c.ID, c.Priority)
+
+	log.Debug().Msgf(
+		"Node %s Updated priority for a message: %d %d %v",
+		f.NodeID,
+		c.ID,
+		c.Priority,
+		err,
+	)
+
+	if err != nil {
+		if err == queue.ErrEmptyQueue {
+			return &FSMResponse{
+				QueueName: c.QueueName,
+				error:     fmt.Errorf("Queue is empty: %s", c.QueueName),
+			}
+		}
+		return &FSMResponse{
+			QueueName: c.QueueName,
+			error:     fmt.Errorf("Failed to dequeue a message from a queue: %s", c.QueueName),
+		}
+	}
+
+	return &FSMResponse{
+		QueueName: c.QueueName,
+		ID:        c.ID,
+		Priority:  c.Priority,
+		Content:   "",
+		error:     nil,
+	}
+}
+
+func (f *FSM) createQueueApply(c Command) *FSMResponse {
+	_, err := f.queueManager.Create(c.QueueType, c.QueueName)
+	if err != nil {
+		return &FSMResponse{
+			QueueName: c.QueueName,
+			error:     fmt.Errorf("Failed to create a queue: %s", c.QueueName),
+		}
+	}
+
+	log.Debug().Msgf("Node %s Created a queue: %s", f.NodeID, c.QueueName)
+	return &FSMResponse{
+		QueueName: c.QueueName,
+		error:     nil,
+	}
+}
+
+func (f *FSM) deleteQueueApply(c Command) *FSMResponse {
+	err := f.queueManager.Delete(c.QueueName)
+	if err != nil {
+		return &FSMResponse{
+			QueueName: c.QueueName,
+			error:     fmt.Errorf("Failed to delete a queue: %s", c.QueueName),
+		}
+	}
+
+	log.Debug().Msgf("Node %s Deleted a queue: %s", f.NodeID, c.QueueName)
+
+	return &FSMResponse{
+		QueueName: c.QueueName,
+		error:     nil,
+	}
 }
 
 func (f *FSM) Snapshot() (raft.FSMSnapshot, error) {
