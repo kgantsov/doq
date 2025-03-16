8263dc4d83d368f6676668605db00a2aca97ae96
Kostiantyn Hantsov <k.gantsov@gmail.com>
2025-03-16 17:19:30 +0100

    Add a test for backup http handler

---
+++ pkg/http/backup_test.go
@@ -0,0 +1,30 @@
+package http
+
+import (
+	"io"
+	"testing"
+
+	"github.com/danielgtaylor/huma/v2/humatest"
+)
+
+func TestBackup(t *testing.T) {
+	_, api := humatest.New(t)
+
+	h := &Handler{
+		node: newTestNode("", true),
+	}
+
+	h.RegisterRoutes(api)
+
+	type ErrorOutput struct {
+		Title  string `json:"title"`
+		Status int    `json:"status"`
+		Detail string `json:"detail"`
+	}
+
+	resp := api.Post("/db/backup", map[string]any{
+		"since": 0,
+	})
+
+	io.Copy(io.Discard, resp.Body)
+}
