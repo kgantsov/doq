6382bc89ec30ac8c1a3a2560895e9cfbb8aa3db0
Kostiantyn Hantsov <k.gantsov@gmail.com>
2025-03-31 20:36:20 +0200

    Remove bytesToUint64 function from storage package

--- pkg/storage/utils.go
+++ pkg/storage/utils.go
@@ -6,11 +6,6 @@ func addPrefix(prefix []byte, key []byte) []byte {
 	return append(prefix, key...)
 }
 
-// Converts bytes to an integer
-func bytesToUint64(b []byte) uint64 {
-	return binary.BigEndian.Uint64(b)
-}
-
 // Converts a uint to a byte slice
 func uint64ToBytes(u uint64) []byte {
 	buf := make([]byte, 8)
