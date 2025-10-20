f4e5598215d1b80401997fc2a0602a7dc040f8f7
Kostiantyn Hantsov <k.gantsov@gmail.com>
2025-10-20 17:31:01 +0200

    Fix load testing script

--- testing/load/queue.js
+++ testing/load/queue.js
@@ -1,19 +1,21 @@
-import http from 'k6/http';
-import { check } from 'k6';
+import http from "k6/http";
+import { check } from "k6";
 
 const queue_name = "autmations";
 const queue_type = "delayed";
 
 export function setup() {
   const url = `http://localhost:8000/API/v1/queues`;
-  let payload = JSON.stringify(
-    {"name": queue_name, "type": queue_type}
-  );
+  let payload = JSON.stringify({
+    name: queue_name,
+    type: queue_type,
+    settings: { max_unacked: 7, strategy: "weighted", ack_timeout: 1800 },
+  });
 
   let params = {
     headers: {
       "Content-Type": "application/json",
-      "Accept": "application/json, application/problem+json"
+      Accept: "application/json, application/problem+json",
     },
     tags: {},
   };
@@ -22,12 +24,11 @@ export function setup() {
 }
 
 export function teardown(data) {
-
   const url = `http://localhost:8000/API/v1/queues/${queue_name}`;
 
   let params = {
     headers: {
-      "Accept": "application/json, application/problem+json"
+      Accept: "application/json, application/problem+json",
     },
     tags: {},
   };
@@ -37,33 +38,35 @@ export function teardown(data) {
 
 export default function () {
   const url = `http://localhost:8000/API/v1/queues/${queue_name}/messages`;
-  let payload = JSON.stringify(
-    {"content": "{\"user_id\": 10}", "priority": 10, "group": "default"}
-  );
+  let payload = JSON.stringify({
+    content: '{"user_id": 10}',
+    priority: 10,
+    group: "default",
+  });
 
   let params = {
     headers: {
       "Content-Type": "application/json",
-      "Accept": "application/json, application/problem+json"
+      Accept: "application/json, application/problem+json",
     },
-    tags: { name: 'enqueue' },
+    tags: { name: "enqueue" },
   };
 
   let resp = http.post(url, payload, params);
 
   check(resp, {
-    'enqueued': (r) => r.status === 200,
+    enqueued: (r) => r.status === 200,
   });
 
   params = {
     headers: {
-      "Accept": "application/json, application/problem+json"
+      Accept: "application/json, application/problem+json",
     },
-    tags: { name: 'dequeue' },
+    tags: { name: "dequeue" },
   };
 
-  resp = http.request("GET", url + '?ack=true', {}, params);
+  resp = http.request("GET", url + "?ack=true", {}, params);
   check(resp, {
-    'dequeued': (r) => r.status === 200,
+    dequeued: (r) => r.status === 200,
   });
 }
